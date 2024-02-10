a9aa9c98dacb2ecd109f9fb1ba3aba87580328b9
Vladimir Semenenko <wsemenenko@gmail.com>
2024-02-10 10:05:52 +0300

    Update code

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -1,22 +1,51 @@
 import os
+import asyncio
 
 import camelot
 import pandas as pd
 
 base_dir = os.path.abspath(os.path.join(__file__, "..", ".."))
 
-def pdf_to_excel(pdf_file_path):
+
+async def pdf_to_excel(pdf_file_path):
     path_to_pdf = os.path.join(base_dir, pdf_file_path)
+        
+    # pages_num = int(input("Enter number of pages: "))
+
+    # if pages_num < 100:
+    #     tables = camelot.read_pdf(path_to_pdf, pages="all")
+    # else:
+    #     step = 100
+    #     count = pages_num
+    #     for i in range(1, pages_num, step):
+    #         tables = camelot.read_pdf(path_to_pdf, pages=f"{i}-{count}")
+    #         count -= 100
+    #         print(count)
+    #         if count < 0:
+    #             step = -count
+    
     tables = camelot.read_pdf(path_to_pdf, pages="all")
 
     combined_df = pd.concat([table.df for table in tables], ignore_index=True)
 
     combined_df.to_excel(
-        pdf_file_path.replace(".pdf", ".xlsx"), sheet_name="Sheet1", index=False
+        os.path.join(base_dir, pdf_file_path.replace(".pdf", ".xlsx")),
+        sheet_name="Sheet1",
+        index=False,
     )
 
 
-if __name__ == "__main__":
+async def main():
+    tasks = []
     for pdf_file_path in os.listdir(base_dir):
         if pdf_file_path.endswith(".pdf"):
-            pdf_to_excel(pdf_file_path)
+            print(f"Converting {pdf_file_path} to Excel format...")
+            tasks.append(pdf_to_excel(pdf_file_path))
+
+    await asyncio.gather(*tasks)
+
+    print("All tasks completed.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
