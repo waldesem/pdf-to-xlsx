63dea3ecbb1726a6a92c1869418e7b912f14f4b8
Vladimir Semenenko <wsemenenko@gmail.com>
2024-02-11 10:00:48 +0300

    Small fix.

--- analyse.py
+++ analyse.py
@@ -3,21 +3,29 @@ import sqlite3
 
 import pandas as pd
 
-base_dir = os.path.abspath(os.path.join(__file__, "..", ".."))
+from pdf_to_excel import base_dir
 
-for xlsx_file_path in os.listdir(base_dir):
-    if xlsx_file_path.endswith(".xlsx"):
-        df = pd.read_excel(
-            os.path.join(base_dir, xlsx_file_path),
-        )
-        
-        headers = [str(col) for col in df.columns]
-        df.columns = headers
 
-        df['0'] = df['0'].astype(int)
-        df['1'] = pd.to_datetime(df['1'], dayfirst=True, format='%d.%m.%y').dt.date
-        df['4'] = pd.to_datetime(df['4'], dayfirst=True, format='%d.%m.%y').dt.date
+def xlsx_to_db(xlsx_file_path):
+    df = pd.read_excel(
+        os.path.join(base_dir, xlsx_file_path),
+    )
 
-        conn = sqlite3.connect(os.path.join(base_dir, xlsx_file_path.replace(".xlsx", ".db")))
+    headers = [str(col) for col in df.columns]
+    df.columns = headers
 
-        df.to_sql('xlsx', conn, if_exists='replace', index=False)
+    df["0"] = df["0"].astype(int)
+    df["1"] = pd.to_datetime(df["1"], dayfirst=True, format="%d.%m.%y").dt.date
+    df["4"] = pd.to_datetime(df["4"], dayfirst=True, format="%d.%m.%y").dt.date
+
+    conn = sqlite3.connect(
+        os.path.join(base_dir, xlsx_file_path.replace(".xlsx", ".db"))
+    )
+
+    df.to_sql("xlsx", conn, if_exists="replace", index=False)
+
+
+if __name__ == "__main__":
+    for xlsx_file_path in os.listdir(base_dir):
+        if xlsx_file_path.endswith(".xlsx"):
+            xlsx_to_db(xlsx_file_path)
